4d147ea0923e4497e0fcc7606b49d4298e485e3e
WeiChien Hsu <weichien711@gmail.com>
2018-09-06 20:26:38 -0500

    :construction: Create Week2-1 folder (Spring Boot)

--- week1/src/main/java/com/spring/uber/service/HelloWorldService.java
+++ week1/src/main/java/com/spring/uber/service/HelloWorldService.java
@@ -1,14 +1,11 @@
 package com.spring.uber.service;
 
 import com.spring.uber.domain.Message;
-import org.springframework.context.annotation.Bean;
-
 
 public class HelloWorldService {
 
-    @Bean
     public Message getMessage(String name, String text) {
-        Message msg = new Message(name, "Hello " );
+        Message msg = new Message(name, "Hello " + name);
         return msg;
     }
 }
