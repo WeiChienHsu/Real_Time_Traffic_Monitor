1a19865b7912b63640fcb19a1d9d82800a84d345
WeiChien Hsu <weichien711@gmail.com>
2018-09-07 16:56:35 -0500

    :construction: Built package and Create Application starter class

--- week2-2/src/main/java/com/kevin/spring/uber/FleetLocationServiceApplication.java
+++ week2-2/src/main/java/com/kevin/spring/uber/FleetLocationServiceApplication.java
@@ -1,5 +1,11 @@
 package com.kevin.spring.uber;
 
-public class FleetLocationServiceApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class FleetLocationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FleetLocationServiceApplication.class, args);
+    }
 }
